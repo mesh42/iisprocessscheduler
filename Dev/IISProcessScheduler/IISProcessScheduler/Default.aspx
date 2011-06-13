4366727a56c5cc07cf77696bf17811be2686745d
cube3_cp <SND\cube3_cp>
2011-06-13 15:21:32 +0000

    initial check-in

---
+++ Dev/IISProcessScheduler/IISProcessScheduler/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="IISProcessScheduler.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblWarmupState" runat="server" Text="lblWarmupState"></asp:Label>
+        <br />
+        <br />
+        <asp:LinkButton ID="lnkPauseResume" runat="server" 
+            onclick="lnkPauseResume_Click">lnkPauseResume</asp:LinkButton>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="lblHistory" runat="server" Text="lblHistory"></asp:Label>
+        <br />
+        <asp:LinkButton ID="lblRefresh" runat="server">Refresh</asp:LinkButton>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Dev/IISProcessScheduler/IISProcessScheduler/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿/* **********************************************************************************
+ *
+ * Copyright (c) TCPX. All rights reserved.
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public
+ * License (Ms-PL). A copy of the license can be found in the license.txt file
+ * included in this distribution.
+ *
+ * You must not remove this notice, or any other, from this software.
+ *
+ * **********************************************************************************/
+using System;
+
+namespace IISProcessScheduler
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (PreWarmUp.Started == DateTime.MinValue)
+            {
+                // Prewarmup not executed.
+                lblWarmupState.Text = "Service is not warmed up.";
+                lnkPauseResume.Text = string.Empty;
+            }
+            else
+            {
+                lblWarmupState.Text = string.Format("Service started at {0}",PreWarmUp.Started);
+                SetLnkText();
+            }
+            lblHistory.Text = string.Empty;
+            foreach (string item in PreWarmUp.LogItems)
+            {
+                lblHistory.Text += item + "<br />";
+            }
+        }
+
+        private void SetLnkText()
+        {
+            lnkPauseResume.Text = PreWarmUp.SchedulingService.IsPaused ? "Resume Scheduler" : "Pause Scheduler";
+        }
+
+        protected void lnkPauseResume_Click(object sender, EventArgs e)
+        {
+            if (PreWarmUp.SchedulingService.IsPaused)
+            {
+                PreWarmUp.SchedulingService.Resume();
+            }
+            else
+            {
+                PreWarmUp.SchedulingService.Pause();
+            }
+            SetLnkText();
+        }
+    }
+}
